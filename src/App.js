dcfa34ccd8b08b5c1662dc8abb7af94cc2b05e38
moizalikhan119 <makstyle119@users.noreply.github.com>
2018-10-20 19:02:33 +0500

    third_comment

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'http://localhost:3000/resumeData.json',
+      url:'https://moizalikhan119.github.io/MAKSTYLE119_Profile/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
