dd7733bf9af1be74c01c64a5b805f0406f5b7e76
moizalikhan119 <38657358+moizalikhan119@users.noreply.github.com>
2018-10-20 18:56:18 +0500

    second_comment
    solve some problem

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'http://localhost:3000/resumeData.json',
+      url:'https://github.com/moizalikhan119/MAKSTYLE119_Profile/blob/master/public/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
